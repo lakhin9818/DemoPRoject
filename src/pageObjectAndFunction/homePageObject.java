9f2482044b89685a8b05a2d9cb72d591267482f8
Lakhin <Lakhin@Lakhin>
2024-11-21 23:29:11 +0530

    Logger and assertions added

---
+++ src/pageObjectAndFunction/homePageObject.java
@@ -0,0 +1,13 @@
+package pageObjectAndFunction;
+
+import org.openqa.selenium.WebElement;
+
+import Base.baseUi;
+
+public class homePageObject extends baseUi {
+
+	public void click(WebElement element) {
+		element.click();
+	}
+
+}
